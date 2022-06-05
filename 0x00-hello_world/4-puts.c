b7c008b7801bb9418bd7a8be4aca95991db21ac3
billy <elbilly257@gmail.com>
2022-06-05 20:26:55 +0200

    billy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*ndabafuse mwaba bono mwe*/
+/*
+*ndabafuse mwaba bono mwe
+*kuber ik mutugola
+*mutuleke
+*/
 int main()
 {
-    puts("\"Programming is like building a multilingual puzzle\n");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
 }
+
 
