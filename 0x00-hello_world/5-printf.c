dd1ae3f5ebfed39cdb91577ec1cc682aeaea3c78
billy <elbilly257@gmail.com>
2022-06-06 23:00:17 +0200

    billy

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+*Description: main - ndabafuse mwaba bono mwe
+*return: 0 kuber ik mutugola
+*mutuleke
+*/
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
