d6e8bb41e0693941bf75144e3ef655cd0eb51b51
billy <elbilly257@gmail.com>
2022-06-06 22:51:47 +0200

    billy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*
-*Description: main - ndabafuse mwaba bono mwe
-*return:  kuber ik mutugola
-*mutuleke
-*/
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
 
 
 
