5340c3b8b9608eaddcd418d00066360af64b11d0
billy <elbilly257@gmail.com>
2022-06-08 19:51:52 +0200

    billy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
+
 /**
  * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Return:0
  */
+
 int main(void)
 {
-	char *sh = "main";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+char c[10] = "putchar";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }
